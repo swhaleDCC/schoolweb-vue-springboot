cce9755055f164f7545ac36a3a2390891e84b762
swhaleDCC <swhale@163.com>
2020-11-27 21:11:14 +0800

    first commit

--- szweb-back/src/main/java/com/haibusiness/szweb/dao/AuthorityDao.java
+++
@@ -1,7 +0,0 @@
-package com.haibusiness.szweb.dao;
-
-import com.haibusiness.szweb.entity.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorityDao extends JpaRepository<Authority,Long> {
-}
--- szweb-back/src/main/java/com/haibusiness/szweb/service/AuthorityService.java
+++
@@ -1,9 +0,0 @@
-package com.haibusiness.szweb.service;
-
-import com.haibusiness.szweb.entity.Authority;
-import org.springframework.stereotype.Repository;
-
-
-public interface AuthorityService {
-    Authority getAuthorityById(Long id);
-}
